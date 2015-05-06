ec3cd794bb90e1977d8fea92c932edcbdfce2fb7
Vitaly Domnikov <dotcypress@gmail.com>
2015-05-06 23:04:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = ((new Uint32Array((new Uint8Array([1,2,3,4])).buffer))[0] === 0x04030201)
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['isLittleEndian'], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.isLittleEndian = factory();
+    }
+}(this, function () {
+    return ((new Uint32Array((new Uint8Array([1,2,3,4])).buffer))[0] === 0x04030201);
+}));
